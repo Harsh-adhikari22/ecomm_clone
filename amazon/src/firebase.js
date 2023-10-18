7a4d3d4ad4a75c8d3646a852c701944c622352c7
harsh adhikari <harshadhikari@harshs-MacBook-Air.local>
2023-10-18 15:40:46 +0530

    c3

---
+++ amazon/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDhy3afAP7mBfkDV_KsCsGC70IxFxNLqUA",
+    authDomain: "clone-e7eaf.firebaseapp.com",
+    projectId: "clone-e7eaf",
+    storageBucket: "clone-e7eaf.appspot.com",
+    messagingSenderId: "692213452607",
+    appId: "1:692213452607:web:8fd12830eb15e946ec0d8a",
+    measurementId: "G-YH97N5YJNT"
+  };
